1de3a22dd6f679b5186574144a11a809c869626a
Justin Cooke <jrcooke2@gmail.com>
2020-07-11 23:51:12 -0300

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, FlatList, Text, Dimensions } from 'react-native';
+import { AppLoading } from "expo";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
-export default function App() {
+const goals = [
+  {
+    key: '1',
+    label: 'Test Goal',
+    milestones: [
+      {
+        label: 'Test Milestore',
+        completed: false
+      },
+      {
+        label: 'Test Milestore 2',
+        completed: true
+      }
+    ]
+  },
+  {
+    key: '2',
+    label: 'Test Goal 2',
+    milestones: [
+      {
+        label: 'Test Milestore',
+        completed: false
+      },
+      {
+        label: 'Test Milestore 2',
+        completed: true
+      }
+    ]
+  },
+  {
+    key: '3',
+    label: 'Test Goal 3',
+    milestones: [
+      {
+        label: 'Test Milestore',
+        completed: false
+      },
+      {
+        label: 'Test Milestore 2',
+        completed: true
+      }
+    ]
+  }
+]
+
+const Header = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.header}>
+      <Text style={styles.text}>My Goals</Text>
     </View>
   );
+};
+
+const GoalCard = (props) => {
+  console.log("GoalCard -> props", props)
+  return (
+    <View style={styles.card}>
+      <Text style={styles.cardHeader}>{props.item.label}</Text>
+    </View>
+  );
+};
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_900Black });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <FlatList
+          data={goals}
+          renderItem={GoalCard}
+          ListHeaderComponent={Header}
+          numColumns={2}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#101010",
+  },
+  header: {
+    paddingLeft: 20,
+    height: 200,
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 30,
+    fontFamily: "Inter_900Black",
+  },
+  card: {
+    backgroundColor: "#424242",
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+    width: (Dimensions.get("window").width / 2) - 20,
+    height: 200
+  },
+  cardHeader: {
+    color: "#fff",
+    fontSize: 20,
+    fontFamily: "Inter_900Black",
   },
 });
